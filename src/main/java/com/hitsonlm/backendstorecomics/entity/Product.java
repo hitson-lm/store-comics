621a00056dac72c67701ccaa089cf6ccfef33cb4
hitson-lm <hitson.lm@gmail.com>
2022-06-22 23:07:43 -0500

    created entity product

---
+++ src/main/java/com/hitsonlm/backendstorecomics/entity/Product.java
@@ -0,0 +1,11 @@
+package com.hitsonlm.backendstorecomics.entity;
+
+public class Product {
+    private String idProducto;
+    private String nombre;
+    private String description;
+    private String tipo;
+    private Double precio;
+    private String imagen;
+    private String fecha;
+}
