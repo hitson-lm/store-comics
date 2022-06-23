bf167794f1aaff7bd2683fc995a97767e7e88b3e
hitson-lm <hitson.lm@gmail.com>
2022-06-22 21:52:15 -0500

    firts commit

---
+++ src/main/java/com/hitsonlm/backendstorecomics/BackendStoreComicsApplication.java
@@ -0,0 +1,13 @@
+package com.hitsonlm.backendstorecomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStoreComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStoreComicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitsonlm/backendstorecomics/BackendStoreComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitsonlm.backendstorecomics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStoreComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
